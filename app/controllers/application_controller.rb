3d32f32d3a5bfaf0f8a990794745f3d57150d2a9
marya314 <mary.boyd314@gmail.com>
2016-08-16 18:03:55 -0400

    Added views, models, controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 require_relative '../../config/environment'
+require 'pry'
 
 class ApplicationController < Sinatra::Base
 
@@ -7,7 +8,62 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do 
-    
+
+  get '/posts/new' do
+      erb :new
   end
+
+  post '/posts' do
+      #binding.pry
+      @post = Post.create(params)
+      #same as (name: params[:name])
+      #hcreate - new and saves post - new and initialize
+      redirect_to '/posts'
+  end
+
+  get '/posts' do
+      if params[:message]
+      @message = params[:message]
+  end
+      @posts = Post.all
+      erb :index
+  end
+
+  get '/posts/:id' do
+
+      @post = Post.find(params[:id])
+      #binding.pry
+      erb :show
+  end
+
+
+  get '/posts/:id/edit' do
+      @post = Post.find(params[:id])
+      erb :edit
+  end
+
+  patch '/posts/:id/edit' do
+      @post = Post.find(params[:id])
+      #@post.name = params[:name]
+      #@post.content = params[:content]
+      @post.update(name: params[:name], content: params[:content])
+
+      #@post.update(params[:name], [:content])
+
+      redirect "/posts/#{@post.id}"
+
+  end
+
+
+    delete '/posts/:id/delete' do
+        @post = Post.find(params[:id])
+        @message = "#{@post.name} was deleted."
+        @post.delete
+        redirect to "/posts?message=#{@message}"
+    end
+
 end
+#redirect from post methods and erb for get methods
+
+
+#why redirect?
